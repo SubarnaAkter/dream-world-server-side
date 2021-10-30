0e40bb9651993c68b5ea5b2bb433f8957b5436cb
subarna <subarnaakter1210@gmail.com>
2021-10-31 03:55:43 +0600

    updated

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ async function run(){
         //update
         app.put('/bookings/:id',async (req,res)=>{
             const id=req.params.id;
-            const updateBooking=req.body;
+          
             const filter ={_id: ObjectId(id)};
             const options = { upsert: true };
             const updateDoc = {
